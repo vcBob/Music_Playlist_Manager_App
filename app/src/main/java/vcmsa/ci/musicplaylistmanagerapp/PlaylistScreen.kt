c5e4785b599f4fa45db697bf1f1e4afa258bf4ac
vcBob <ST10480829@vcconnect.edu.za>
2025-06-19 10:46:41 +0200

    Display button created in Details screen.

--- app/src/main/java/vcmsa/ci/musicplaylistmanagerapp/PlaylistScreen.kt
+++ app/src/main/java/vcmsa/ci/musicplaylistmanagerapp/PlaylistScreen.kt
@@ -45,9 +45,9 @@ class MainActivity : AppCompatActivity() {
         //==========================================================================================
         // The code below declares variables for arrays and has pre determined data for the user to have examples of what to add.
         val i = 4 // 'i' is used to represent index.
-        val arrSong = arrayOf("One Dance", "Alright","January 28th","Snooze")
+        val arrSong =  arrayOf("One Dance", "Alright","January 28th","Snooze")
         val arrArtist = arrayOf("Drake", "Kendrick Lamar", "J.Cole", "SZA")
-        val arrRating = arrayOf(4,5,5,4)
+        val arrRating = ArrayList<Int>()// arrayOf(4,5,5,4)
         val arrComment = arrayOf("The song is electrifying and makes me dance.","This song makes bad feelings go away","What and experience we got taken on", "This is a great Love song.")
         //==========================================================================================
 
@@ -84,6 +84,10 @@ class MainActivity : AppCompatActivity() {
 
         details.setOnClickListener{
             val playlistToDetails = Intent(this, DetailsDisplay::class.java)
+            playlistToDetails.putExtra("song", arrSong)
+            playlistToDetails.putExtra("artist", arrArtist)
+            playlistToDetails.putIntegerArrayListExtra("rating", arrRating)
+            playlistToDetails.putExtra("comment", arrComment)
             startActivity(playlistToDetails)
         }
 
