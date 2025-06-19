fe6db2646749472145f1fec5398da7c59ba10d15
vcBob <ST10480829@vcconnect.edu.za>
2025-06-19 08:20:46 +0200

    Rename Playlist Screen and Added 2 Buttons

---
+++ app/src/main/java/vcmsa/ci/musicplaylistmanagerapp/PlaylistScreen.kt
@@ -0,0 +1,13 @@
+package vcmsa.ci.musicplaylistmanagerapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.playlist_screen)
+    }
+}
