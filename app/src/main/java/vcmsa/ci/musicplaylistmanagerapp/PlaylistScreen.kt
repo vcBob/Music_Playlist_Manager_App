daefc47c10e5fcc5641dce2ec8e99f557ab30256
vcBob <ST10480829@vcconnect.edu.za>
2025-06-19 10:15:27 +0200

    Code to show user what data to enter and exit button.

--- app/src/main/java/vcmsa/ci/musicplaylistmanagerapp/PlaylistScreen.kt
+++ app/src/main/java/vcmsa/ci/musicplaylistmanagerapp/PlaylistScreen.kt
@@ -18,24 +18,64 @@ class MainActivity : AppCompatActivity() {
         val playlist = findViewById<Button>(R.id.btnPlaylist)
         val welcome = findViewById<TextView>(R.id.txtWelcome)
 
-        // code for calling song details.
+        // Code that calls the song details.
         val song = findViewById<Button>(R.id.btnSong)
         val txtSong = findViewById<TextView>(R.id.edtSong)
+
+        // Code that calls the artist details.
+        val artist = findViewById<Button>(R.id.btnArtist)
+        val txtArtist = findViewById<TextView>(R.id.edtArtist)
+
+        // Code that calls the song rating details.
+        val rating = findViewById<Button>(R.id.btnRating)
+        val txtRating = findViewById<TextView>(R.id.edtRating)
+
+        // Code that calls the users comment details.
+        val comment = findViewById<Button>(R.id.btnComment)
+        val txtComment = findViewById<TextView>(R.id.edtComment)
+
+        val exit = findViewById<Button>(R.id.btnExit)
         //==========================================================================================
 
         //==========================================================================================
-        // The code below declares variables for arrays and has pre determened data for the user to have examples of what to add.
-        val i = 3 // 'i' is used to represent index.
+        // The code below declares variables for arrays and has pre determined data for the user to have examples of what to add.
+        val i = 4 // 'i' is used to represent index.
         val arrSong = arrayOf("One Dance", "Alright","January 28th","Snooze")
         val arrArtist = arrayOf("Drake", "Kendrick Lamar", "J.Cole", "SZA")
         val arrRating = arrayOf(4,5,5,4)
-        val arrComment = arrayOf("The song is electrifying and makes me dance.","This song makes bad feelings go away","What and experiance we got taken on", "This is a great Love song.")
+        val arrComment = arrayOf("The song is electrifying and makes me dance.","This song makes bad feelings go away","What and experience we got taken on", "This is a great Love song.")
         //==========================================================================================
 
         //==========================================================================================
+        // The code below requires the user to click the 'Add Playlist Button' so that it shows the user the first piece of data they are required to fill in.
         playlist.setOnClickListener{
             song.setVisibility(View.VISIBLE)
             txtSong.setVisibility(View.VISIBLE) }
+
+        song.setOnClickListener{
+            //txtSong.text=arrSong[i]
+            artist.setVisibility(View.VISIBLE)
+            txtArtist.setVisibility(View.VISIBLE)
+        }
+
+        artist.setOnClickListener{
+            //txtArtist.text=arrSong[i]
+            rating.setVisibility(View.VISIBLE)
+            txtRating.setVisibility(View.VISIBLE)
+        }
+
+        rating.setOnClickListener{
+            //txtRating.text=arrSong[i]
+            comment.setVisibility(View.VISIBLE)
+            txtComment.setVisibility(View.VISIBLE)
+        }
+
+        //==========================================================================================
+
+        //==========================================================================================
+        //The code below allows to user to exit the app.
+        exit.setOnClickListener{finishAffinity()}
+        //==========================================================================================
 
     }
 }
