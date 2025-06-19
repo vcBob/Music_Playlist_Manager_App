82bd4509d5dd149b3d1cc660e1db0fddee988d5f
vcBob <ST10480829@vcconnect.edu.za>
2025-06-19 09:19:56 +0200

    Populated array variables and and playlist button to ask user for song details after clicking.

--- app/src/main/java/vcmsa/ci/musicplaylistmanagerapp/PlaylistScreen.kt
+++ app/src/main/java/vcmsa/ci/musicplaylistmanagerapp/PlaylistScreen.kt
@@ -1,6 +1,7 @@
 package vcmsa.ci.musicplaylistmanagerapp
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -12,7 +13,29 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.playlist_screen)
 
+        //==========================================================================================
+        // The code below uses the function 'findviewbyid' to identify components by there id names and call them from the .xml folder to the .kt folder of the playlist screen folder.
         val playlist = findViewById<Button>(R.id.btnPlaylist)
         val welcome = findViewById<TextView>(R.id.txtWelcome)
+
+        // code for calling song details.
+        val song = findViewById<Button>(R.id.btnSong)
+        val txtSong = findViewById<TextView>(R.id.edtSong)
+        //==========================================================================================
+
+        //==========================================================================================
+        // The code below declares variables for arrays and has pre determened data for the user to have examples of what to add.
+        val i = 3 // 'i' is used to represent index.
+        val arrSong = arrayOf("One Dance", "Alright","January 28th","Snooze")
+        val arrArtist = arrayOf("Drake", "Kendrick Lamar", "J.Cole", "SZA")
+        val arrRating = arrayOf(4,5,5,4)
+        val arrComment = arrayOf("The song is electrifying and makes me dance.","This song makes bad feelings go away","What and experiance we got taken on", "This is a great Love song.")
+        //==========================================================================================
+
+        //==========================================================================================
+        playlist.setOnClickListener{
+            song.setVisibility(View.VISIBLE)
+            txtSong.setVisibility(View.VISIBLE) }
+
     }
 }
